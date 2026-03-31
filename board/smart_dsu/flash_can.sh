87c7370fcf8984336e5edec95d8b2f2f0ddbfdce
Scott Chamberlain <scottchamberlain@MBPM1.local>
2026-03-30 23:21:10 -0500

    Fix libusb1 version, flash_can.sh path, add gc-sections flags for GCC 13.x

--- board/smart_dsu/flash_can.sh
+++ board/smart_dsu/flash_can.sh
@@ -5,4 +5,4 @@ cd ..
 SDSU=1 scons -u
 cd smart_dsu
 
-../../tests/gateway/enter_canloader.py ../obj/panda.bin.signed
+../../tests/pedal/enter_canloader.py ../obj/panda.bin.signed
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
   platforms='any',
   license='MIT',
   install_requires=[
-    'libusb1 == 1.6.6',
+    'libusb1 >= 3.0.0',
     'hexdump >= 3.3',
     'pycryptodome >= 3.9.8',
     'tqdm >= 4.14.0',
